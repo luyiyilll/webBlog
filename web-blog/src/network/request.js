eb5a5443961d8733df58b2e7244cb9c0f1381dec
1224847912 <1224847912@qq.com>
2020-06-11 09:56:20 +0800

    路由修改

--- web-blog/src/network/request.js
+++ web-blog/src/network/request.js
@@ -2,6 +2,7 @@ import axios from 'axios'
 export function request (config, success, failure) {
   const instance = axios.create({
     baseURL: 'http://123.56.121.160:8085',
+    // baseURL: 'http://localhost:8085',
     timeout: 5000,
 
 
