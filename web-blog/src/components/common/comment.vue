e22164cd9d8ec017407934b5703f49ad1a939cce
Liu Song <1154553312@qq.com>
2020-06-15 13:32:13 +0800

    remove gitTest.txt

---
+++ web-blog/src/components/common/comment.vue
@@ -0,0 +1,19 @@
+<!--<template>-->
+<!--    <div v-bind:id="vcomments">-->
+
+<!--    </div>-->
+<!--</template>-->
+
+<!--<script>-->
+
+<!--    const Valine = require('valine');-->
+
+<!--    export default {-->
+<!--        name: "comment"-->
+
+<!--    }-->
+<!--</script>-->
+
+<!--<style scoped>-->
+
+<!--</style>-->
